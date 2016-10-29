6ee39e9755996867d098f89ec5488f978ebe755d
Utkarsh Gupta <utkarsh357@gmail.com>
2016-10-29 06:47:58 +0530

    Initial work on booth multiplier

--- multi_8_bit.v
+++ multi_8_bit.v
@@ -19,8 +19,29 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module multi_8_bit(
+	//a : multiplicand
+	//b : multiplier
+	//p : product
 	input [7:0] a,
 	input [7:0] b,
 	output [15:0] p
 );
+	reg [16:0] tmp_a, tmp_a_2comp, tmp_p;
+	reg [7:0] a_2comp;
+
+	always @(a or b) begin
+		//Calculating the two's complement of the multiplicand.
+		a_2comp = ((~ a) + 1'b1);
+		tmp_a = {a, 5'b00000};
+		tmp_a_2comp = {a_2comp, 5'b00000};
+		
+		//Temporary product initial value
+		tmp_p = {4'b0000, b, 1'b0};		
+		
+		
+	
+	end	
+	
+		
+	
 endmodule
